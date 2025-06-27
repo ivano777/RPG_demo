5c7b32bd310f9170723780cba464a92c1c3a3d8b
Ivano D'Onofrio <ivano.donofrio@igt.com>
2025-06-27 10:01:27 +0200

    controller launch4j

--- controller/src/main/java/dminis/rpg/game/configuration/BrowserLauncher.java
+++ controller/src/main/java/dminis/rpg/game/configuration/BrowserLauncher.java
@@ -2,20 +2,35 @@ package dminis.rpg.game.configuration;
 
 import java.awt.Desktop;
 import java.net.URI;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class BrowserLauncher {
+
+    @Value("${rpg-demo.deploy-end.url:http://localhost:8080}")
+    private String frontEndUrl;
 
     @EventListener(ApplicationReadyEvent.class)
     public void openBrowser() {
         try {
-            Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                    desktop.browse(new URI(frontEndUrl));
+                } else {
+                    log.error("Azione BROWSE non supportata");
+                }
+            } else {
+                log.error("Desktop non supportato");
+            }
         } catch (Exception e) {
-            // se fallisce non muore l'app, logga e basta
-            System.err.println("Impossibile aprire il browser: " + e.getMessage());
+            log.error("Errore apertura browser:",e);
         }
     }
 }
