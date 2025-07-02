a0baaf60b1c9622076e54bc5484fb9d841035b44
Ivano D'Onofrio <ivano.donofrio@igt.com>
2025-07-02 11:25:58 +0200

    added init enemy script for h2

---
+++ controller/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+MERGE INTO enemy (name)          KEY(name) VALUES ('Goblin');
+MERGE INTO enemy (name)          KEY(name) VALUES ('Orc');
+MERGE INTO enemy (name)          KEY(name) VALUES ('Skeleton');
+MERGE INTO enemy (name)          KEY(name) VALUES ('Dark Magician');
+MERGE INTO enemy (name)          KEY(name) VALUES ('Kangaroo');
